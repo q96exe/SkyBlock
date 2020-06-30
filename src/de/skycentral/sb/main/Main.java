2218947e26f233e9549e73acc3112a3d6fe1792e
q96 <marvin.brueckner@gmx.de>
2020-06-30 22:18:41 +0200

    First classes

--- src/de/skycentral/sb/main/Main.java
+++ src/de/skycentral/sb/main/Main.java
@@ -1,8 +1,32 @@
 package de.skycentral.sb.main;
 
+import de.skycentral.sb.utils.Data;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    
+    public Main instance;
 
+    @Override
+    public void onEnable() {
+        instance = this;
+        init();
+        Bukkit.getConsoleSender().sendMessage(Data.prefix + "§aDas Plugin wurde erfolgreich aktiviert");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(Data.prefix + "§cDas Plugin wurde erfolgreich deaktiviert");
+    }
+
+    private void init() {
+        //COMMANDS
+
+        //EVENTS
+
+    }
+
+    public Main getInstance() {
+        return instance;
+    }
 }
---
+++ src/de/skycentral/sb/utils/Data.java
@@ -0,0 +1,8 @@
+package de.skycentral.sb.utils;
+
+public class Data {
+
+    public static final String prefix = "";
+    public static final String noperms = prefix + "";
+    public static final String use = prefix + "";
+}
