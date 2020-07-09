6635ede90ded10e745bd018c71076a3f53bef03d
Marvin Brückner <marvin.brueckner@gmx.net>
2020-07-09 22:12:57 +0200

    IslandGUI and CreateIslandGUI

---
+++ src/de/skycentral/sb/gui/CreateIsland.java
@@ -0,0 +1,26 @@
+package de.skycentral.sb.gui;
+
+public class CreateIsland {
+
+    private String name;
+
+    public CreateIsland(String schematicName) {
+        this.name = schematicName;
+    }
+
+    public void createIsland() {
+
+    }
+
+    public void teleportToIsland() {
+
+    }
+
+    public void deleteIsland() {
+
+    }
+
+    public void listIslands() {
+        
+    }
+}
--- src/de/skycentral/sb/gui/IslandGUI.java
+++ src/de/skycentral/sb/gui/IslandGUI.java
@@ -5,12 +5,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class IslandGUI {
 
     public IslandGUI() {
 
     }
+
+    ItemStack platzhalter = new ItemBuilder(Material.BARRIER, 1).setDisplayname("§c§lPlatzhalter").build();
 
     public void openIslandGUI(Player player) {
         Inventory inventory = Bukkit.createInventory(null, 9 * 3, "§a§lIsland");
@@ -20,6 +23,9 @@ public class IslandGUI {
         }
 
         inventory.setItem(10, new ItemBuilder(Material.GRASS).setDisplayname("§a§lIsland Menü").build());
+        inventory.setItem(12, platzhalter);
+        inventory.setItem(14, platzhalter);
+        inventory.setItem(16, platzhalter);
 
         player.openInventory(inventory);
     }
